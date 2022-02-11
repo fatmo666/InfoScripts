fb42814cc49a470b1b19a0891da647cfdb853a57
fatmo666 <1069030824@qq.com>
2022-02-12 04:37:17 +0800

    完成端口扫描功能

--- PortScanner.py
+++ PortScanner.py
@@ -20,6 +20,7 @@ class PortScanner(BaseObject):
         args = self.argparser()
         # 生成主域名列表，待检测域名入队
         target = args.target
+        self.ports = args.port
         if not os.path.isfile(target):
             # target = 'http://' + target
             self.domains.append(target)
@@ -30,18 +31,32 @@ class PortScanner(BaseObject):
                     if not domain.startswith(('http://', 'https://')):
                         self.domains.append(domain)
 
+    def argparser(self):
+        """
+        解析参数
+        :return:参数解析结果
+        """
+        parser = argparse.ArgumentParser(description='InfoScripts can help you collect target\'s information',
+                                         epilog='\tUsage:\npython3 ' + sys.argv[0] + " --target www.baidu.com -p 80,8080")
+        parser.add_argument('--target', '-t', help='A target like www.example.com or subdomains.txt', required=True)
+        parser.add_argument('--port', '-p', help='the ports you want to scan', required=True)
+        args = parser.parse_args()
+        return args
+
     def startQuery(self):
         try:
             tasks = []
             newLoop = asyncio.new_event_loop()
             asyncio.set_event_loop(newLoop)
             loop = asyncio.get_event_loop()
+            sem = asyncio.Semaphore(256)
+            scanner = aionmap.PortScanner()
 
             for domain in self.domains:
                 if os.path.exists(os.getcwd() + '/result/' + domain + '/') is False:
                     os.mkdir(os.getcwd() + '/result/' + domain + '/')
 
-                tasks.append(asyncio.ensure_future(self.httpScan(domain)))
+                tasks.append(asyncio.ensure_future(self.scan(domain, sem, scanner)))
 
             loop.run_until_complete(asyncio.wait(tasks))
         except KeyboardInterrupt:
@@ -51,20 +66,36 @@ class PortScanner(BaseObject):
 
         self.writeResult()
 
-    async def httpScan(self, domain):
-        scanner = aionmap.PortScanner()
-        print(await scanner.nmap_version())
-        result = await scanner.scan(domain, None, '-p 80,7777')
-        print(result)
+    async def scan(self, domain, sem, scanner):
+        async with sem:
+            result = await scanner.scan(domain, None, '-p {}'.format(self.ports))
+            for item in result.hosts[0].services:
+                if item.state == 'open':
+                    if domain not in self.queryResult.keys():
+                        self.queryResult[domain] = {}
+                    if 'open' not in self.queryResult[domain].keys():
+                        self.queryResult[domain]["open"] = []
+                    self.queryResult[domain]["open"].append(item.port)
+                elif item.state == 'filtered':
+                    if domain not in self.queryResult.keys():
+                        self.queryResult[domain] = {}
+                    if 'filtered' not in self.queryResult[domain].keys():
+                        self.queryResult[domain]["filtered"] = []
+                    self.queryResult[domain]["filtered"].append(item.port)
 
     def writeResult(self):
-        with open('./CheckResult/' + self.fileName + "/" + 'http' + '.txt', 'a') as fp:
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/CheckResult/' + self.fileName + "/" + 'port-open' + '.txt', 'a') as fp:
             for domain in self.domains:
-                pass
-                # fp.write(domain + "\n")
+                for port in self.queryResult[domain]["open"]:
+                    fp.write(domain + "|" + str(port) + '\r\n')
 
-                # with open(os.getcwd() + '/result/' + domain + '/' + 'HostUpInfo.json', 'w') as fpResult:
-                #     json.dump(self.queryResult[domain], fpResult, indent=2)
+                with open(os.path.dirname(os.path.abspath(__file__)) + '/result/' + domain + '/' + 'ports.json', 'w') as fpResult:
+                    json.dump(self.queryResult[domain], fpResult, indent=2)
+
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/CheckResult/' + self.fileName + "/" + 'port-filtered' + '.txt', 'a') as fp:
+            for domain in self.domains:
+                for port in self.queryResult[domain]["filtered"]:
+                    fp.write(domain + "|" + str(port) + '\r\n')
 
 
 if __name__ == '__main__':
